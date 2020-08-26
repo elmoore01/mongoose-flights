255648249ad4015224665b635f7fd2d914e68ca8
Eric Moore <eric@educate-ca.org>
2020-08-25 19:59:09 -0700

    commit 1

---
+++ models/flight.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Delta', 'Southwest','United'],
+        required: true, 
+        unique: true,
+    },
+    
+    airport: {
+        type: String,
+        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+        required: true,
+        unique: true,
+    },
+
+    flightNo: {
+        type: Number,
+        min: [10],
+        max: [9999],
+        required: true,
+        unique: true,
+    },
+
+    departs: {
+        type: Date,
+        default: Date.now + 365 Days, 
+        required: true,
+        unique: true,
+    },
+})
