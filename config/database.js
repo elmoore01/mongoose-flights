be390db1756000e83847d1f78154770942ae1b78
Eric Moore <eric@educate-ca.org>
2020-08-26 17:06:25 -0700

    connect db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/flights',
-{ useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect('mongodb://localhost/flights', { 
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useCreateIndex: true
+});
 
 const db = mongoose.connection;
 
